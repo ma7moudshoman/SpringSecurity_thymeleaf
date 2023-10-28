7eccb96f9bc9e5e5a79506b26959c95c01c775df
ma7moudshoman <shomanm53@gmail.com>
2023-10-29 00:28:09 +0200

    login

--- src/main/java/registerlogin/login/Config/SecurityConfiguration.java
+++ src/main/java/registerlogin/login/Config/SecurityConfiguration.java
@@ -14,7 +14,7 @@ import registerlogin.login.Service.UserService;
 
 @Configuration
 @EnableWebSecurity
- public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+ public  class  SecurityConfiguration extends WebSecurityConfigurerAdapter{
 @Autowired
     private  UserService userService;
     @Bean
